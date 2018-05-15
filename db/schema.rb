1f51cebb182e656bead989e5aff1368bf179260e
Germain Loret <germain.loret@gmail.com>
2018-05-15 09:21:08 +0200

    added address to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_071039) do
+ActiveRecord::Schema.define(version: 2018_05_15_071757) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
